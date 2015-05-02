a84206820c34f55d4bda9d4738151167fe87b48f
Sarah Christ <schrist810@gmail.com>
2015-05-02 10:14:24 -0500

    image updates

--- homepage.php
+++ homepage.php
@@ -6,7 +6,7 @@ get_header(); ?>
 
 <body>
   <div class="main-content">
-
+    <?php get_template_directory_uri() . '/images/cupcakes.jpg' ?>
     <p> Hey ya’ll! I’m Sarah of Sarah’s Sweets! The exclusive <q>bakery</q> catering to the employees of Heiman Fire Equipment and family members of Sarah Christ.  Take a peek at some of the sweets I’ve created, and head to the <a href=contact.html>contact</a> page to get in touch.
   </p>
 </div>
@@ -20,13 +20,13 @@ get_header(); ?>
         <div class="cupcakes">
        <h2>Cupcakes</h2>
         <p>
-          <img src="images/cupcakes1.jpg" alt="Cupcakes"></a>
+          
         </p>
       </div>
         <div class="contact">
       <h2>Contact &amp; Pricing</h2>
       <p>
-        <img src="images/money.jpg" alt="Pricing"></a>
+
       </p>
     </div>
 
